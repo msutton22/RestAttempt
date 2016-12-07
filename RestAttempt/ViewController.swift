b813d9db8462fbab8d3badfde5e71862c1890bca
Maddy Sutton <student@msutton.local>
2016-12-07 09:19:50 -0600

    Initial Commit

---
+++ RestAttempt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestAttempt
+//
+//  Created by Maddy Sutton on 12/7/16.
+//  Copyright © 2016 Sutton Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
